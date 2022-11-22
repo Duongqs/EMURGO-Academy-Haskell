ae8ac5970402672985ca4ffaf81f12070a7b79a9
Nguyen Ngoc Duong <ngocduong87@gmail.com>
2022-11-22 23:21:59 +0000

    +A1 answer and play with Sandbox

--- hello.hs
+++ hello.hs
@@ -1,1 +1,12 @@
-double x=x+x
+--fizzBuzz :: Int -> String 
+--fizzBuzz x =
+--    if (x `mod` 3 ==0 && x `mod` 5 ==0) then "FizzBuzz"
+--    else if (x `mod` 3 ==0 ) then "Fizz"
+--    else if (x `mod` 5 ==0) then "Buzz"
+--    else show x
+power :: Integer -> Integer 
+power x | x == 0 = 1 -- 1st guard
+       | x /= 0 = x * x -- 2nd guard
+main = do 
+   putStrLn "The square of 10 is:"  -- Adding text for the output
+   print (power 10) -- printing to screen    
--- solutions/Sandbox.hs
+++ solutions/Sandbox.hs
@@ -1,1 +1,21 @@
 module Sandbox where
+import Text.Read (Lexeme(String, Char))
+import Foreign (toBool)
+half :: Int -> Double
+half n = (fromIntegral n)/2.0
+
+fizzBuzz :: Int -> String
+fizzBuzz x =
+    if (x `mod` 3 == 0 && x `mod` 5 == 0 ) then "FizzBuzz"
+    else if (x `mod` 3 == 0 ) then "Fizz"
+    else if (x `mod` 5 == 0) then "Buzz"
+    else show x
+
+fizzBuzz2 :: Integer -> String
+fizzBuzz2 x
+  | (x `mod` 3 == 0 && x `mod` 5 == 0 ) = "FizzBuzz"
+  | (x `mod` 3 == 0 ) = "Fizz"
+  | (x `mod` 5 == 0) = "Buzz"
+  | otherwise = show x
+--allTrueF :: Bool -> (Bool -> Bool)
+--allTrueF x y = (x == True and y == True)
